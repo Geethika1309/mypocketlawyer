19d58b84d631ea3f6a74072989145c775050de70
Geethika1309 <marujollageethika08@gmail.com>
2025-08-22 16:06:53 +0530

    Update languageManager.js

--- languageManager.js
+++ languageManager.js
@@ -122,7 +122,7 @@ class LanguageManager {
                    'askemail':'askemail',
                    'askquestion':'askquestion',
                    'asksend':'asksend',
-                   'lcheading':'lcheading','lcdiv1':'lcdiv1','lcdiv2':'lcdiv2','lcdiv3':'lcdiv3','lcdiv4':'lcdiv4','lcdiv5':'lcdiv5','lcdiv6':'lcdiv6','lcdiv7':'lcdiv7','lcdiv8':'lcdiv8','lcdiv9':'lcdiv9','lcdiv10':'lcdiv10','lcdiv11':'lcdiv11','lcdiv12':'lcdiv12','lcdiv13':'lcdiv13','lcdiv14':'lcdiv14','lcdiv15':'lcdiv15','lcdiv16':'lcdiv16','lcdiv17':'lcdiv17','lcdiv18':'lcdiv18','lcdiv19':'lcdiv19','lcdiv20':'lcdiv20','lcdiv21':'lcdiv21','lcdiv22':'lcdiv22','lcdiv23':'lcdiv23','lcdiv24':'lcdiv24',
+                   'lcheading':'lcheading','lcdiv1':'lcdiv1','lcdiv2':'lcdiv2','lcdiv3':'lcdiv3','lcdiv4':'lcdiv4','lcdiv5':'lcdiv5','lcdiv6':'lcdiv6','lcdiv7':'lcdiv7','lcdiv8':'lcdiv8','lcdiv9':'lcdiv9','lcdiv10':'lcdiv10','lcdiv11':'lcdiv11','lcdiv12':'lcdiv12','lcdiv13':'lcdiv13','lcdiv14':'lcdiv14','lcdiv15':'lcdiv15','lcdiv16':'lcdiv16','lcdiv17':'lcdiv17','lcdiv18':'lcdiv18','lcdiv19':'lcdiv19','lcdiv20':'lcdiv20','lcdiv21':'lcdiv21','lcdiv22':'lcdiv22','lcdiv23':'lcdiv23','lcdiv24':'lcdiv24','lcdiv25':'lcdiv25','lcdiv26':'lcdiv26','lcdiv27':'lcdiv27','lcdiv28':'lcdiv28','lcdiv29':'lcdiv29','lcdiv30':'lcdiv30',
                    'generateDocHeading':'generateDocHeading',
                    'contactheading':'contactheading',
                    'contactplace':'contactplace',
@@ -188,3 +188,4 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
 });
+
